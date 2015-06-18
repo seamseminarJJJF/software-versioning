c3b88503c50616df76c19facf881df96e9d3c5a3
juan <jmflorezff@gmail.com>
2015-06-18 13:29:01 -0500

    - Added properties, getters and setters for User class.

--- src/User.java
+++ src/User.java
@@ -2,4 +2,49 @@
  * Created by juan on 6/18/15.
  */
 public class User {
+    private String name;
+    private  String email;
+    private String username;
+    private String password;
+    private String loginStatus;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(String loginStatus) {
+        this.loginStatus = loginStatus;
+    }
 }
