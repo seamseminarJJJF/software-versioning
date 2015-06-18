7cd308698e70fa1c2b5ca4c651e9b5054ef1a8be
Juan José <jj.diaz1067@uniandes.edu.co>
2015-06-18 17:59:45 -0500

    The getters and setters of Product were implemented

--- src/Product.java
+++ src/Product.java
@@ -7,4 +7,36 @@ public class Product {
     private String category;
     private String description;
     private long price;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
 }
