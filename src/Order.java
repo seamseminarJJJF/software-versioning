6d775db510339d13859f1a174a5282310a3a16a4
Juan José <jj.diaz1067@uniandes.edu.co>
2015-06-18 17:30:47 -0500

    The atributes of class order were implemented

--- src/Order.java
+++ src/Order.java
@@ -1,5 +1,45 @@
+import java.util.Date;
+
 /**
  * Created by juan on 6/18/15.
+ *
  */
 public class Order {
+
+    private int orderId;
+    private Date creationDate;
+    private String dateShipped;
+    private String status;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getDateShipped() {
+        return dateShipped;
+    }
+
+    public void setDateShipped(String dateShipped) {
+        this.dateShipped = dateShipped;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
