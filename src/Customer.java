d2eea84b6014103dc766eaa7c8d7ebaab324b1d5
Juan José <jj.diaz1067@uniandes.edu.co>
2015-06-18 17:26:59 -0500

    Class customer was completed

--- src/Customer.java
+++ src/Customer.java
@@ -1,5 +1,50 @@
 /**
  * Created by juan on 6/18/15.
+ *
  */
-public class Customer {
+public class Customer extends User{
+
+    private long creditCard;
+    private String[] interests;
+    private String postalAddress;
+
+    public void addInterest(String interest){
+        for(int i=0;i<interests.length;i++){
+            if(interests[i]==null){
+                interests[i]= interest;
+            }
+        }
+    }
+
+    private void removeInterest(String interest){
+        for(int i=0;i<interests.length;i++){
+            if(interests[i].equals(interest)){
+                interests[i] = null;
+            }
+        }
+    }
+
+    public long getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(long creditCard) {
+        this.creditCard = creditCard;
+    }
+
+    public String[] getInterests() {
+        return interests;
+    }
+
+    public void setInterests(String[] interests) {
+        this.interests = interests;
+    }
+
+    public String getPostalAddress() {
+        return postalAddress;
+    }
+
+    public void setPostalAddress(String postalAddress) {
+        this.postalAddress = postalAddress;
+    }
 }
