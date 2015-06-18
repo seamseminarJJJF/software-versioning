ff78fad279f2d4b7c1dcf430bc3f3ec1c54b6744
juan <jmflorezff@gmail.com>
2015-06-18 11:59:44 -0500

    - Added attributes, getters and setters for class Administrator.

--- src/Administrator.java
+++ src/Administrator.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by juan on 6/18/15.
  */
 public class Administrator {
+    private List<String> permissions = new ArrayList<String>();
+    private List<String> roles = new ArrayList<String>();
+
+    public List<String> getPermissions() {
+        return permissions;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    public void setPermissions(List<String> permissions) {
+        this.permissions = permissions;
+    }
 }
