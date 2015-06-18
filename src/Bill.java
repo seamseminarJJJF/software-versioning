11d75987c76ba9079d8a3f920a8cee2b626afc64
Juan José <jj.diaz1067@uniandes.edu.co>
2015-06-18 18:15:19 -0500

    Some methods of order were changed

--- src/Bill.java
+++ src/Bill.java
@@ -5,29 +5,4 @@
 public class Bill {
     private long totalCost;
     private int[] discounts;
-
-    public long getTotalCost() {
-        return totalCost;
-    }
-
-    public void setTotalCost(long totalCost) {
-        this.totalCost = totalCost;
-    }
-
-    public int[] getDiscounts() {
-        return discounts;
-    }
-
-    public void setDiscounts(int[] discounts) {
-        this.discounts = discounts;
-    }
-
-    public long calculateTotalCost(){
-        int desc = 0;
-        for (int i = 0; i<discounts.length;i++){
-            desc += discounts[i];
-        }
-        return totalCost*(desc/100);
-
-    }
 }
