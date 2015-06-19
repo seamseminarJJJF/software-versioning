294e9b2a729978bd64d7f7c203b4981d936d6c61
juan <jmflorezff@gmail.com>
2015-06-19 07:28:54 -0500

    - Added getters and setters for class Product.

--- src/Product.java
+++ src/Product.java
@@ -8,12 +8,42 @@ public class Product {
     private String description;
     private long price;
 
-
-
     public Product(int productId, String category, String description, long price) {
         this.productId = productId;
         this.category = category;
         this.description = description;
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
         this.price = price;
     }
 }
